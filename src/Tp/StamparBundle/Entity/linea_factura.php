c5f97f37696e3cfed71bbba9684790fffa87d728
fabian <fabian@fabian-945GCT-M>
2012-11-21 20:59:53 -0300

    entity linea_factura, factura, Producto completos

---
+++ src/Tp/StamparBundle/Entity/linea_factura.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Tp\StamparBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * linea_factura
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class linea_factura
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="cantidad", type="float")
+     */
+    private $cantidad;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="total", type="float")
+     */
+    private $total;
+    
+    /**
+     * @var Factura
+     *
+     * @ORM\ManyToOne(targetEntity="factura", inversedBy="idLineaFactura")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_factura", referencedColumnName="id", onDelete="CASCADE")
+     * })
+     */
+    private $idFactura;
+
+    /**
+     * @var Producto
+     *
+     * @ORM\ManyToOne(targetEntity="Producto")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_producto", referencedColumnName="id")
+     * })
+     */
+    private $idProducto;
+    
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set cantidad
+     *
+     * @param float $cantidad
+     * @return linea_factura
+     */
+    public function setCantidad($cantidad)
+    {
+        $this->cantidad = $cantidad;
+    
+        return $this;
+    }
+
+    /**
+     * Get cantidad
+     *
+     * @return float 
+     */
+    public function getCantidad()
+    {
+        return $this->cantidad;
+    }
+
+    /**
+     * Set total
+     *
+     * @param float $total
+     * @return linea_factura
+     */
+    public function setTotal($total)
+    {
+        $this->total = $total;
+    
+        return $this;
+    }
+
+    /**
+     * Get total
+     *
+     * @return float 
+     */
+    public function getTotal()
+    {
+        return $this->total;
+    }
+    
+    /**
+     * Set idFactura
+     *
+     * @param Tp\StamparBundle\Entity\factura $idFactura
+     * @return LineaFactura
+     */
+    public function setIdFactura(\Tp\StamparBundle\Entity\factura $idFactura = null)
+    {
+        $this->idFactura = $idFactura;
+    
+        return $this;
+    }
+
+    /**
+     * Get idFactura
+     *
+     * @return \Tp\StamparBundle\Entity\factura
+     */
+    public function getIdFactura()
+    {
+        return $this->idFactura;
+    }
+    
+    /**
+     * Set idProducto
+     *
+     * @param Tp\StamparBundle\Entity\Producto $idProducto
+     * @return linea_factura
+     */
+    public function setIdProducto(\Tp\StamparBundle\Entity\Producto $idProducto = null)
+    {
+        $this->idProducto = $idProducto;
+    
+        return $this;
+    }
+
+    /**
+     * Get idProducto
+     *
+     * @return Tp\StamparBundle\Entity\Producto
+     */
+    public function getIdProducto()
+    {
+        return $this->idProducto;
+    }
+}
