e8572dbad58f481c0f9ed6cc55730ad312f01dd8
Gonzalo Alonso <gonkpo@gmail.com>
2012-11-21 18:53:23 -0300

    entitys

---
+++ src/Tp/StamparBundle/Entity/LineaPedido.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tp\StamparBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Tp\StamparBundle\Entity\LineaPedido
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class LineaPedido
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer $cantidad
+     *
+     * @ORM\Column(name="cantidad", type="integer")
+     */
+    private $cantidad;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set cantidad
+     *
+     * @param integer $cantidad
+     * @return LineaPedido
+     */
+    public function setCantidad($cantidad)
+    {
+        $this->cantidad = $cantidad;
+    
+        return $this;
+    }
+
+    /**
+     * Get cantidad
+     *
+     * @return integer 
+     */
+    public function getCantidad()
+    {
+        return $this->cantidad;
+    }
+}
---
+++ src/Tp/StamparBundle/Entity/Pedido.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tp\StamparBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Tp\StamparBundle\Entity\Pedido
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Pedido
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime $fecha
+     *
+     * @ORM\Column(name="fecha", type="datetime")
+     */
+    private $fecha;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set fecha
+     *
+     * @param \DateTime $fecha
+     * @return Pedido
+     */
+    public function setFecha($fecha)
+    {
+        $this->fecha = $fecha;
+    
+        return $this;
+    }
+
+    /**
+     * Get fecha
+     *
+     * @return \DateTime 
+     */
+    public function getFecha()
+    {
+        return $this->fecha;
+    }
+}
